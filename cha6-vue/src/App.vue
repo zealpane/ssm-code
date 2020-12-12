67377c48736c422b20736c3a734b30d4fe14d8ab
泽朋 <zealpane@163.com>
2020-12-12 19:56:08 +0800

    语音提示

--- cha6-vue/src/App.vue
+++ cha6-vue/src/App.vue
@@ -1,11 +1,139 @@
 <template>
   <div id="app">
     <router-view />
+    <audio id="myaudio" :src="aSource" controls="controls" loop="false" hidden="true" />
   </div>
 </template>
 
 <script>
+// 可以在这里导入request，然后服务器配置DeferedResult，使其延迟返回；等服务器返回时再向服务器请求
+import io from 'socket.io-client'
+import axios from 'axios'
+import { alarmType } from '@/utils/constant'
+
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      alarmType: alarmType,
+      cmdStatus: {
+        'DELIVERED': '已送达',
+        'PENDING': '等待中',
+        'SENT': '已发送'
+      },
+      myAuto: null,
+      aSource: '/static/audio/10344.mp3'
+    }
+  },
+  mounted() {
+    // this.$notify({
+    //   title: '有新的告警信息',
+    //   dangerouslyUseHTMLString: true,
+    //   message: this.alarmType[0],
+    //   type: 'warning',
+    //   duration: 0,
+    //   onClose: () => {
+    //     this.myAuto.pause()
+    //   }
+    // })
+    // const key = `open${Date.now()}`
+    // this.$notification.open({
+    //   message: 'Notification Title',
+    //   description: 'A function will be be called after the notification is closed (automatically after the "duration" time of manually).',
+    //   btn: (h) => {
+    //     return h('a-button', {
+    //       props: {
+    //         type: 'primary',
+    //         size: 'small'
+    //       },
+    //       on: {
+    //         click: () => this.$notification.close(key)
+    //       }
+    //     }, 'Confirm')
+    //   },
+    //   key,
+    //   onClose: close
+    // })
+    this.myAuto = document.getElementById('myaudio')
+    // this.myAuto.play()
+
+    axios.get('/api/user/member/selfInfo').then(res => {
+      if (res.data.code !== 0) {
+        return
+      }
+      // 这里要提示还有几条告警信息待处理
+      const socket = io('http://ws.iot.gdatacloud.com:8868?no=' + res.data.content, {
+        transports: ['websocket']
+      })
+      socket.on('connect', () => {
+        console.log('connect')
+      })
+      socket.on('commandRsp', (data) => {
+        console.log(data)
+        this.$notify({
+          title: '指令发送状态变化',
+          message: this.cmdStatus[data.result.resultCode],
+          type: 'info'
+        })
+      })
+      socket.on('workOrderEvent', () => {
+        this.$notify({
+          title: '有新的工单任务',
+          // message: this.cmdStatus[data.result.resultCode],
+          type: 'info'
+        })
+      })
+      // 告警推送
+      socket.on('alarmEvent', (data) => {
+        console.log('接收到告警信息', data)
+
+        // this.aSource = 'http://tsn.baidu.com/text2audio?lan=zh&ctp=1&cuid=abcdxxx&tok=24.7f2bf541d211c6a0d5f8d51c7c642d34.2592000.1558256775.282335-16041202&tex=有新的告警信息&vol=9&per=0&spd=5&pit=5&aue=3'
+        this.$notify({
+          title: '有新的告警信息',
+          dangerouslyUseHTMLString: true,
+          message: this.alarmType[data.alarmType],
+          type: 'warning',
+          // duration: 0,
+          onClose: () => {
+            this.myAuto.pause()
+          }
+        })
+        this.myAuto.play()
+        console.log('alarmEvent', data)
+      })
+      // 巡检推送
+      socket.on('inspectEvent', (data) => {
+        console.log('接收到巡检任务', data)
+        const jsonData = JSON.parse(data)
+        // this.aSource = 'http://tsn.baidu.com/text2audio?lan=zh&ctp=1&cuid=abcdxxx&tok=24.7f2bf541d211c6a0d5f8d51c7c642d34.2592000.1558256775.282335-16041202&tex=有新的告警信息&vol=9&per=0&spd=5&pit=5&aue=3'
+        this.$notify({
+          title: jsonData.title,
+          dangerouslyUseHTMLString: true,
+          message: jsonData.content,
+          type: 'info',
+          duration: 0,
+          onClose: () => {
+            this.myAuto.pause()
+          }
+        })
+      })
+      socket.on('disconnect', () => {
+        console.log('disconnect')
+      })
+    })
+    if (!window.EventSource) {
+      console.error('不支持SSE')
+      // var source = new EventSource('')
+    }
+  }
 }
 </script>
+
+<style>
+body{
+  background-color: #f0f2f5;
+}
+.a-card{
+  margin: 15px;
+}
+</style>
